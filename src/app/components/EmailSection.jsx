f75a5d63a9fcfc6479758e427f8fa30025da2f32
Sanya Kapoor <sanyakapoor2705@gmail.com>
2024-01-28 12:39:58 +0530

    website changes

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -59,7 +59,7 @@ const EmailSection = () => {
           <Link href="https://github.com/sanyakapoor27">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com/in/sanya-k-494584261">
+          <Link href="https://www.linkedin.com/in/sanya-k-494584261/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
