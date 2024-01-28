d62246e52298e4e01420d8308f6846d02dd24f0b
Sanya Kapoor <sanyakapoor2705@gmail.com>
2024-01-28 12:20:46 +0530

    website changes

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -47,7 +47,7 @@ const HeroSection = () => {
               Contact Me
             </Link>
             <Link
-              href="/"
+              href="https://docs.google.com/document/d/1AeRSsBz47cp2Jq-6FZT075I8zcPQZk4lnGueQAPM9Ss/edit?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
